356c9dc23a996abce4cf357110f252bd5c7e8e98
slaxmankiran <slaxmankiran@gmail.com>
2024-07-13 07:04:44 -0400

    Updated py code added

--- Day-06/operators_assignment.py
+++ Day-06/operators_assignment.py
@@ -12,17 +12,12 @@ product = a * b
 
 quotient = a / b
 
-<<<<<<< HEAD
+
 print("Sum is:", (sum))
 print("Difference is:" , (diff))
 print("Product is:", (product))
 print("Quotient is: ", (quotient))
-=======
-print("Sum is:", sum)
-print("Difference is:" , diff)
-print("Product is:", product)
-print("Quotient is: ", quotient)
->>>>>>> d757f88 (Updated py files)
+
 
 ## Task 2: Comparison Operators
 
@@ -33,21 +28,14 @@ comp4 = a >= b
 comp5 = c == b
 comp6 = c != b
 
-<<<<<<< HEAD
+
 print("Comparison1 a < b is: ", (comp1))
 print("Comparison2 a > b is: ", (comp2))
 print("Comparison3 a<=b is: ", (comp3))
 print("Comparison4 a>=b is: ", (comp4))
 print("Comparison5 c==b is: ", (comp5))
 print("Comparison6 c!=b is: ", (comp6))
-=======
-print("Comparison1 a < b is: ", comp1)
-print("Comparison2 a > b is: ", comp2)
-print("Comparison3 a<=b is: ", comp3)
-print("Comparison4 a>=b is: ", comp4)
-print("Comparison5 c==b is: ", comp5)
-print("Comparison6 c!=b is: ", comp6)
->>>>>>> d757f88 (Updated py files)
+
 
 ## Task 3: Logical Operators
 
@@ -60,17 +48,12 @@ result_not_x = not x
 result_not_y = not y
 
 
-<<<<<<< HEAD
+
 print("Result_and is: ", (result_and))
 print("Result_or is: ", (result_or))
 print("Result_not_x is: ", (result_not_x))
 print("Result_not_y is: ", (result_not_y))
-=======
-print("Result_and is: ", result_and)
-print("Result_or is: ", result_or)
-print("Result_not_x is: ", result_not_x)
-print("Result_not_y is: ", result_not_y)
->>>>>>> d757f88 (Updated py files)
+
 
 ## Task 4: Assignment Operators
 
@@ -80,11 +63,9 @@ total -= 3
 total *= 2
 total /= 4
 
-<<<<<<< HEAD
+
 print("Final total is: ", (total))
-=======
-print("Final total is: ", total)
->>>>>>> d757f88 (Updated py files)
+
 
 ## Task 5: Bitwise Operators (Optional)
 
@@ -97,21 +78,15 @@ b = ["apple", "ball", "cat", "dog"]
 idenity_is = a is b
 idenity_is_not = a is not b
 
-<<<<<<< HEAD
+
 print("Identity a is b: ", (idenity_is))
 print("Identity a is not b: ", (idenity_is_not))
-=======
-print("Identity a is b: ", idenity_is)
-print("Identity a is not b: ", idenity_is_not)
->>>>>>> d757f88 (Updated py files)
+
 
 membership_in = "ball" in my_list
 membership_not_in = "elephant" not in my_list
 
-<<<<<<< HEAD
+
 print("Ball in my_list: ", (membership_in))
 print("Elephant is not in my_list: ", (membership_not_in))
-=======
-print("Ball is in my_list: ", membership_in)
-print("Elephant is not in my_list: ", membership_not_in)
->>>>>>> d757f88 (Updated py files)
+
